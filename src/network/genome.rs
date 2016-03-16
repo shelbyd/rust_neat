3ad6857b229d26103cba1e600c77d0570c498d40
Shelby Doolittle <shelbyd@google.com>
2016-03-15 21:55:04 -0700

    add disabled genes

--- src/network/genome.rs
+++ src/network/genome.rs
@@ -27,6 +27,7 @@ impl Genome {
                          .collect(),
             edges: self.genes
                        .iter()
+                       .filter(|gene| gene.enabled)
                        .map(|gene| {
                            Edge::new(Node::new(gene.in_index),
                                      Node::new(gene.out_index),
@@ -37,10 +38,12 @@ impl Genome {
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct Gene {
     in_index: usize,
     out_index: usize,
     weight: f64,
+    enabled: bool,
 }
 
 impl Gene {
@@ -49,7 +52,14 @@ impl Gene {
             in_index: in_index,
             out_index: out_index,
             weight: weight,
+            enabled: true,
         }
+    }
+
+    fn disabled(&self) -> Gene {
+        let mut gene = self.clone();
+        gene.enabled = false;
+        gene
     }
 }
 
@@ -71,8 +81,8 @@ mod test {
         let network = genome.to_network();
 
         assert_eq!(network.edges.len(), 2);
-        assert_eq!(network.edges[0], Edge::new(Node::new(0), Node::new(2), 0.7));
-        assert_eq!(network.edges[1], Edge::new(Node::new(1), Node::new(2), 0.5));
+        assert!(network.edges.contains(&Edge::new(Node::new(0), Node::new(2), 0.7)));
+        assert!(network.edges.contains(&Edge::new(Node::new(1), Node::new(2), 0.5)));
 
         // One sensor is for the bias node.
         assert_eq!(network.sensors.len(), 2);
@@ -81,5 +91,20 @@ mod test {
 
         assert_eq!(network.outputs.len(), 1);
         assert_eq!(network.outputs[0], Node::new(2));
+    }
+
+    #[test]
+    fn test_with_disabled_gene() {
+        // When an edge is split.
+        let genes = vec![
+                        Gene::new(0, 2, 0.7, 1),
+                        Gene::new(1, 2, 0.5, 2).disabled(),
+                        Gene::new(1, 3, 0.9, 3),
+                        Gene::new(3, 2, 0.6, 4),
+                    ];
+        let genome = Genome::new(1, 1, genes);
+        let network = genome.to_network();
+
+        assert_eq!(network.edges.len(), 3);
     }
 }
